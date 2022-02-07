5092f0de0f6a3387f3bd4fce1002bdb531b5bfd0
Pablo Correa Gómez <ablocorrea@hotmail.com>
2022-02-07 23:04:41 +0100

    tests: initial implementation

---
+++ tests/gs-self-test.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2022 Pablo Correa Gomez <ablocorrea@hotmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#include <gnome-software.h>
+
+#include <gs-plugin-loader-sync.h>
+#include <gs-plugin-loader.h>
+#include <gs-test.h>
+
+/* static void */
+/* gs_plugin_refresh_func (void) */
+/* { */
+/* } */
+
+/* static void */
+/* gs_plugin_add_updates_func (void) */
+/* { */
+/* } */
+
+/* static void */
+/* gs_plugin_update_func (void) */
+/* { */
+/* } */
+
+/* static void */
+/* gs_plugin_adopt_app_func (void) */
+/* { */
+/* } */
+
+/* static void */
+/* gs_plugin_refine_func (void) */
+/* { */
+/* } */
+
+/* static void */
+/* gs_plugin_add_sources (void) */
+/* { */
+/* } */
+
+static void
+gs_plugins_apk_app_install_remove (GsPluginLoader *plugin_loader)
+{
+  g_autoptr (GError) error = NULL;
+  g_autoptr (GsPluginJob) plugin_job = NULL;
+  g_autoptr (GsApp) app = NULL;
+  gboolean rc;
+
+  // Create installable app
+  app = gs_app_new ("dev.test");
+  gs_app_set_state (app, GS_APP_STATE_AVAILABLE);
+  gs_app_set_kind (app, AS_COMPONENT_KIND_GENERIC);
+  gs_app_set_metadata (app, "apk::name", "test");
+  gs_app_set_management_plugin (app, "apk");
+
+  // Execute installation action
+  plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_INSTALL,
+                                   "app", app,
+                                   NULL);
+  rc = gs_plugin_loader_job_action (plugin_loader, plugin_job, NULL, &error);
+  gs_test_flush_main_context ();
+  g_assert_no_error (error);
+  g_assert_true (rc);
+
+  // Verify app is now installed
+  g_assert_cmpint (gs_app_get_state (app), ==, GS_APP_STATE_INSTALLED);
+
+  // Execute remove action
+  g_object_unref (plugin_job);
+  plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_REMOVE,
+                                   "app", app,
+                                   NULL);
+  rc = gs_plugin_loader_job_action (plugin_loader, plugin_job, NULL, &error);
+  gs_test_flush_main_context ();
+  g_assert_no_error (error);
+  g_assert_true (rc);
+
+  // Verify app is now removed
+  g_assert_cmpint (gs_app_get_state (app), ==, GS_APP_STATE_AVAILABLE);
+}
+
+int
+main (int argc, char **argv)
+{
+  g_autoptr (GsPluginLoader) plugin_loader = NULL;
+  g_autoptr (GSettings) settings = NULL;
+  g_autoptr (GError) error = NULL;
+  gboolean ret;
+  int retval;
+  const gchar *allowlist[] = {
+    "apk",
+    NULL
+  };
+
+  settings = g_settings_new ("org.gnome.software");
+  /* Avoid connections to review server during tests */
+  g_assert_true (g_settings_set_string (settings, "review-server", ""));
+  /* We do not want real data to pollute tests.
+   * Might be useful at some point though */
+  g_assert_true (g_settings_set_strv (settings, "external-appstream-urls", NULL));
+
+  g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
+
+  g_test_init (&argc, &argv,
+               G_TEST_OPTION_ISOLATE_DIRS,
+               NULL);
+
+  /* only critical and error are fatal */
+  g_log_set_fatal_mask (NULL, G_LOG_LEVEL_WARNING |
+                                  G_LOG_LEVEL_ERROR |
+                                  G_LOG_LEVEL_CRITICAL);
+
+  /* we can only load this once per process */
+  plugin_loader = gs_plugin_loader_new ();
+  /* g_signal_connect (plugin_loader, "status-changed", */
+  /*                   G_CALLBACK (gs_plugin_loader_status_changed_cb), NULL); */
+  gs_plugin_loader_add_location (plugin_loader, LOCALPLUGINDIR);
+  ret = gs_plugin_loader_setup (plugin_loader,
+                                (gchar **) allowlist,
+                                NULL,
+                                NULL,
+                                &error);
+  g_assert_no_error (error);
+  g_assert_true (ret);
+  g_assert_true (gs_plugin_loader_get_enabled (plugin_loader, "apk"));
+
+  g_test_add_data_func ("/gnome-software/plugins/apk/app-install-remove",
+                        plugin_loader,
+                        (GTestDataFunc) gs_plugins_apk_app_install_remove);
+
+  retval = g_test_run ();
+
+  /* Clean up. */ /* Probably not needed */
+  /* gs_utils_rmtree (tmp_root, NULL); */
+
+  return retval;
+}
---
+++ tests/test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2022 Pablo Correa Gomez <ablocorrea@hotmail.com>
+#
+# SPDX-License-Identifier: GPL-2.0+
+
+import dbus
+from dbusmock import DBusTestCase, MOCK_IFACE
+import os
+import subprocess
+import sys
+import unittest
+
+class GsPluginApkTest (DBusTestCase):
+    @classmethod
+    def setUpClass(klass):
+        klass.start_system_bus()
+        klass.dbus_con = klass.get_dbus(system_bus=True)
+
+    def setUp(self):
+        self.p_mock = self.spawn_server('dev.Cogitri.apkPolkit1',
+                                        '/dev/Cogitri/apkPolkit1',
+                                        'dev.Cogitri.apkPolkit1',
+                                        system_bus=True,
+                                        stdout=subprocess.PIPE) # This should be changed to a file!
+
+        self.apk_polkit_mock = dbus.Interface(self.dbus_con.
+                                              get_object('dev.Cogitri.apkPolkit1',
+                                                         '/dev/Cogitri/apkPolkit1'),
+                                              MOCK_IFACE)
+
+        self.apk_polkit_mock.AddMethods('', [
+            ('AddPackage', 's', '', ''),
+            ('DeletePackage', 's', '', '')
+        ])
+
+
+    def tearDown(self):
+        self.p_mock.stdout.close()
+        self.p_mock.terminate()
+        self.p_mock.wait()
+
+    def test_apk(self):
+        builddir = os.getenv('G_TEST_BUILDDIR')
+        test_file = os.path.join(builddir, 'gs-self-test-apk')
+        result = subprocess.run(test_file)
+        self.assertEqual(result.returncode, 0)
+
+
+if __name__ == '__main__':
+    _test = unittest.TextTestRunner(stream=sys.stdout)
+    unittest.main(testRunner=_test)
